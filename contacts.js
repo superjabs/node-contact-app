dbc6434017852d6b97fccc97b573e08a54264e64
alfian <alfiandev8@gmail.com>
2023-01-04 09:57:09 +0700

    add a chalk  and validator package

--- contacts.js
+++ contacts.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const readLine = require('readline');
+const chalk = require('chalk');
+const validator = require('validator');
 
 // membuat folder jika belum ada
 const dirPath = './data';
@@ -13,32 +14,44 @@ if(!fs.existsSync(dataPath)){
   fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const rl = readLine.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
-
-const tulisPertanyaan = (pertanyaan) => {
-  return new Promise((resolve, rejects) => {
-    rl.question(pertanyaan, (nama) => {
-      resolve(nama);
-    });
-  });
-};
 
 const simpanContact = (nama, email, noHp) => {
   const contact = {nama, email, noHp};
   const file = fs.readFileSync('data/contacts.json', 'utf-8');
   const contacts = JSON.parse(file);
+
+  // cek duplikasi
+  const duplikasi = contacts.find((contact) => contact.nama === nama);
+  if(duplikasi){
+    console.log(chalk.bgRed.bold('data sudah ada, silahkan ganti yang lain!'));
+    return false;
+  }
+
+  // validasi email
+  if(email){
+    if(!validator.isEmail(email)){
+      console.log(
+        chalk.bgRed('email tidak valid!')
+        );
+        return false;
+    };
+  };
+
+  // validasi nomor hp
+  if(noHp){
+    if(!validator.isMobilePhone(noHp, 'id-ID')){
+      console.log(
+        chalk.bgRed('nomor hape tidak valid!')
+        );
+        return false;
+    };
+  };
+
   contacts.push (contact)
   fs.writeFileSync('data/contacts.json', JSON.stringify(contacts, null,2));
 
-  console.log(`terima kasih ${nama}, sudah memasukkan data!`)
-  rl.close();
+  console.log(chalk.bgGreen.white(`terima kasih ${nama}, sudah memasukkan data!`));
 };
 
-module.exports = {
-  tulisPertanyaan,
-  simpanContact
-}
+module.exports = { simpanContact }
