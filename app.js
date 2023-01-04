9b1a8319d309c1bbf0fed0309c96239505dfb711
alfian <alfiandev8@gmail.com>
2023-01-04 10:03:23 +0700

    add yargs package

--- app.js
+++ app.js
@@ -1,48 +1,53 @@
-const { rejects } = require('assert');
-const fs = require('fs');
-const { resolve } = require('path');
-const readLine = require('readline');
+// mengambil argumen dari command line
+const yargs = require('yargs');
+const { simpanContact } = require('./contacts');
 
-// membuat folder
-const dirPath = './data';
-if(!fs.existsSync(dirPath)){
-  fs.mkdirSync(dirPath);
-}
-
-// membuat  file contact.json di dalam folder data
-const dataPath = './data/contacts.json';
-if(!fs.existsSync(dataPath)){
-  fs.writeFileSync(dataPath, '[]', 'utf-8');
-}
-
-const rl = readLine.createInterface({
-  input: process.stdin,
-  output: process.stdout
+yargs.command({
+  command: 'add',
+  describe: 'Menambahkan contact baru',
+  builder: {
+    nama: {
+      describe: 'Nama lengkap',
+      demandOption: true,
+      type: 'string'
+    },
+    email: {
+      describe: 'Email Aktif',
+      demandOption: false,
+      type: 'string'
+    },
+    noHP: {
+      describe: 'No HP Aktif',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    simpanContact(argv.nama, argv.email, argv.noHP);
+  }
 });
+yargs.parse();
 
 
-const tulisPertanyaan = (pertanyaan) => {
-  return new Promise((resolve, rejects) => {
-    rl.question(pertanyaan, (nama) => {
-      resolve(nama);
-    });
-  });
-};
 
 
-const main = async () => {
-  const nama = await tulisPertanyaan('Masukkan nama anda : ');
-  const email = await tulisPertanyaan('Masukkan email anda : ');
-  const noHp = await tulisPertanyaan('Masukkan nomor hp anda : ');
 
-  const contact = {nama, email, noHp};
-  const file = fs.readFileSync('data/contacts.json', 'utf-8');
-  const contacts = JSON.parse(file);
-  contacts.push (contact)
-  fs.writeFileSync('data/contacts.json', JSON.stringify(contacts, null,2));
 
-  console.log(`terima kasih ${nama}, sudah memasukkan data!`)
-  rl.close();
-}
 
-main();
+
+
+
+
+
+
+// const { simpanContact, tulisPertanyaan } = require("./contacts");
+
+// const main = async () => {
+//   const nama = await tulisPertanyaan('Masukkan nama anda : ');
+//   const email = await tulisPertanyaan('Masukkan email anda : ');
+//   const noHp = await tulisPertanyaan('Masukkan nomor hp anda : ');
+
+//   simpanContact(nama, email, noHp);
+// };
+
+// main();
