f18a91840ab5838654e03bdd4341b40b7a636f22
alfian <alfiandev8@gmail.com>
2023-01-04 12:48:47 +0700

    add method to get and remove data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // mengambil argumen dari command line
 const yargs = require('yargs');
-const { simpanContact } = require('./contacts');
+const { simpanContact, listContact, detailContact, deleteContact } = require('./contacts');
 
 yargs.command({
   command: 'add',
@@ -25,7 +25,46 @@ yargs.command({
   handler(argv) {
     simpanContact(argv.nama, argv.email, argv.noHP);
   }
+}).demandCommand();
+
+yargs.command({
+  command: 'list',
+  describe: 'Menampilkan semua nama & no hp contact',
+  handler() {
+    listContact();
+  }
 });
+
+yargs.command({
+  command: 'detail',
+  describe: 'Menampilkan detail kontak berdasarkan nama',
+  builder: {
+    nama: {
+      describe: 'Nama lengkap',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    detailContact(argv.nama);
+  }
+})
+
+yargs.command({
+  command: 'delete',
+  describe: 'Menghapus kontak',
+  builder: {
+    nama: {
+      describe: 'Nama lengkap',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    deleteContact(argv.nama);
+  }
+})
+
 yargs.parse();
 
 
