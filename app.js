48fa5e00ebaab769019260c58b1c94173bc84cf2
alfian <alfiandev8@gmail.com>
2023-01-09 06:36:38 +0700

    remove comment code

--- app.js
+++ app.js
@@ -63,30 +63,6 @@ yargs.command({
   handler(argv) {
     deleteContact(argv.nama);
   }
-})
+});
 
 yargs.parse();
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const { simpanContact, tulisPertanyaan } = require("./contacts");
-
-// const main = async () => {
-//   const nama = await tulisPertanyaan('Masukkan nama anda : ');
-//   const email = await tulisPertanyaan('Masukkan email anda : ');
-//   const noHp = await tulisPertanyaan('Masukkan nomor hp anda : ');
-
-//   simpanContact(nama, email, noHp);
-// };
-
-// main();
